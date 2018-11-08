0a3838f32ede3d2234e6d713a8f36f49f04c8217
unknown <ogi762@gmail.com>
2018-11-08 20:08:52 +0700

    tugas

---
+++ day6/if else.js
@@ -0,0 +1,11 @@
+var i = 30;
+
+for(i =  3; <= 30; i+=3){
+    if(i % 15 === 0){
+        console.log('HACKFOX = ' + i);
+    } else if (i % 6 === 0){
+        console.log('FOX = ' + i);
+    } else {
+        console.log('HACK = i + i);
+    }
+}
---
+++ day6/looping.js
@@ -0,0 +1,6 @@
+for(var i = 1; <= 100; i ++) {
+    if (i % 3 === 1)
+    console.log("Angka Genap" + i);
+    else
+    console.log("Angka Ganjil" + i)
+}
